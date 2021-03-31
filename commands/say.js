aca837b3b1c374b1522b24136ecd4c7ff0498fa4
BuildTools <unconfigured@null.spigotmc.org>
2021-03-31 16:06:54 -0300

    change code structure uwu owo?

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`<:SW_pandapolicia:767092860800991252> **»** **Você não tem permissão para usar este comando!**`)
+/* module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission("BAN_MEMBERS"))
+    return message.reply(`<:SW_pandapolicia:767092860800991252> **»** **Você não tem permissão para usar este comando!**`)
   const sayMessage = args.join(' ');
-  message.delete().catch(O_o => {});
+  message.delete().catch(err => {});
   message.channel.send(sayMessage);
-}; 
+
+}; */
+module.exports = {
+  name: 'say',
+  alisases: ['falar'],
+  description: 'Faz o bot falar qualquer coisa!',
+
+  async execute(message, args, client) {
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+      return message.reply(`<:SW_pandapolicia:767092860800991252> **»** **Você não tem permissão para usar este comando!**`)
+    const sayMessage = args.join(' ');
+    message.delete().catch(err => {});
+    message.channel.send(sayMessage);
+  }
+}; //para boceta
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
 app.get("/", (request, response) => {
   const ping = new Date();
@@ -11,41 +12,54 @@ app.listen(process.env.PORT); // Recebe solicitações que o deixa online
 const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json");
-
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  console.log(`O arquivo ${command} foi carregado corretamente!`)
+}
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (message.channel.type == 'dm') return;
-  
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (config.blacklistedChannels.includes(message.channel.id) && !config.devs.includes(message.author.id)) 
+    return message.reply(`<:SW_pandapolicia:767092860800991252> **»** **Você não tem permissão para usar este comando aqui!**`)
   if (!message.content.startsWith(process.env.PREFIX.toLowerCase())) return;
-  if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+  if (!message.content.startsWith(`<@!${client.user.id}>`) || !message.content.startsWith(`<@${client.user.id}>`)) return;
   const args = message.content
-    .trim().slice(process.env.PREFIX.length)
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
-
+    .slice(process.env.PREFIX.length)
+    .split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command = client.commands.get(commandName) ||
+    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  if (!command) return;
   try {
-    const commandFile = require(`./commands/${command}.js`)
     commandFile.run(client, message, args);
     const embed = new Discord.MessageEmbed()
       .setTitle("OwO")
       .setColor("fff3f")
       .setDescription("dicc")
       .addField("<:certoooooooooo:767092932959797331>  **» Quem Utilizou:**", message.author.tag)
-      .addField("<:staff:825830974088019968> » Mensagem Completa",`${message.content}`)
+      .addField("<:staff:825830974088019968> » Mensagem Completa", `${message.content}`)
       .setFooter(new Date(message.createdTimestamp));
-      message.reply(embed)
+    message.guild.channels.cache.get('826152829412311081').send(embed);
   } catch (err) {
     console.error('Erro:' + err);
+    const embed = new MessageEmbed()
+      .setTitle('OOF')
+      .setDescription('```' + err.stack + '```')
+      .setColor('#c62b1d')
+      .setTimestamp(new Date());
+    message.channel.send(embed);
   }
 });
 
 client.on("ready", () => {
-  let activities = [
-    `Utilize ${process.env.PREFIX}help para obter ajuda`,
-    `O Servidor crescer rapidamente!`,
-    `O Whiteee e He1Korno me Progamar!`,
-    `Os Usuários se divertindo no chat!`
-  ],
+  const activities = [
+      `Utilize ${process.env.PREFIX}help para obter ajuda`,
+      `O Servidor crescer rapidamente!`,
+      `O Whiteee e He1Korno me Progamar!`,
+      `Os Usuários se divertindo no chat!`
+    ];
     i = 0;
   setInterval(() => client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, {
     type: "WATCHING"
@@ -56,4 +70,4 @@ client.on("ready", () => {
   console.log("Estou Online!")
 });
 
-client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token NjY0ODU0NzU4NjM2NTE5NDc0.XhdIPg.VOgwtVag4nuh_mDTALm_Uo6vKx0
+client.login(process.env.TOKEN);
